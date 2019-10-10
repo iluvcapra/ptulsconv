3e6e2b5776dc52b85ef2c42eee74c8348ac00141
Jamie Hardt <jamiehardt@me.com>
2019-10-09 21:13:47 -0700

    Update __init__.py
    v0.0.2

--- ptulsconv/__init__.py
+++ ptulsconv/__init__.py
@@ -2,6 +2,6 @@ from .ptuls_grammar import protools_text_export_grammar
 from .ptuls_parser_visitor import DictionaryParserVisitor
 from .transformations import TimecodeInterpreter
 
-__version__ = '0.0.1'
+__version__ = '0.0.2'
 __author__  = 'Jamie Hardt'
 __license__ = 'MIT'

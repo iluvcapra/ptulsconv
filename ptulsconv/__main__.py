a68adca6b1b5a641760e9504d62aa4cb2016e26c
Jamie Hardt <jamiehardt@me.com>
2019-10-09 20:03:18 -0700

    Reformatted file

--- ptulsconv/__main__.py
+++ ptulsconv/__main__.py
@@ -2,20 +2,23 @@ from ptulsconv.commands import convert
 from optparse import OptionParser
 import sys
 
+
 def main():
     parser = OptionParser()
     parser.usage = "ptulsconv TEXT_EXPORT.txt"
     parser.add_option('-i', dest='in_time', help="Don't output events occurring before this timecode, and offset"
-					     " all events relative to this timecode.", metavar='TC')
+                                                 " all events relative to this timecode.", metavar='TC')
     parser.add_option('-o', dest='out_time', help="Don't output events occurring after this timecode.", metavar='TC')
-    parser.add_option('-P','--progress', default=False, action='store_true', dest='show_progress', help='Show progress bar.')
-    parser.add_option('-m','--include-muted', default=False, action='store_true', dest='include_muted', help='Read muted clips.')
+    parser.add_option('-P', '--progress', default=False, action='store_true', dest='show_progress',
+                      help='Show progress bar.')
+    parser.add_option('-m', '--include-muted', default=False, action='store_true', dest='include_muted',
+                      help='Read muted clips.')
 
     (options, args) = parser.parse_args(sys.argv)
     if len(args) < 2:
-        print("Error: No input file",file=sys.stderr)
+        print("Error: No input file", file=sys.stderr)
         parser.print_help(sys.stderr)
-	sys.exit(-1)
+        sys.exit(-1)
 
     convert(input_file=args[1], start=options.in_time, end=options.out_time, include_muted=options.include_muted,
             progress=options.show_progress, output=sys.stdout)
@@ -23,4 +26,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-

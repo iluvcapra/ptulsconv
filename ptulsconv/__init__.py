73936510cdf236015589252bf4865ad42362d4a7
Jamie Hardt <jamiehardt@me.com>
2019-11-12 17:34:09 -0800

    Changed appearance of error reports

--- ptulsconv/__init__.py
+++ ptulsconv/__init__.py
@@ -2,6 +2,6 @@ from .ptuls_grammar import protools_text_export_grammar
 from .ptuls_parser_visitor import DictionaryParserVisitor
 from .transformations import TimecodeInterpreter
 
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 __author__  = 'Jamie Hardt'
 __license__ = 'MIT'

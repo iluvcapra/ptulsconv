6ca1e5532ddd27a052ff01c8a6f4d5fbe1c564f3
Jamie Hardt <jamiehardt@me.com>
2020-10-10 23:14:57 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(name='ptulsconv',
       keywords='text-processing parsers film tv editing editorial',
       install_requires=['parsimonious', 'tqdm'],
       package_data={
-          "": ["*.xsl"]
+          "ptulsconv": ["xslt/*.xsl"]
       },
       entry_points={
           'console_scripts': [

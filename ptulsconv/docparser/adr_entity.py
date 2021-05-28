2cc4f423cf75cc321c09d677254a5833d95ed137
Jamie Hardt <jamiehardt@me.com>
2021-05-27 23:08:20 -0700

    Type annotations

--- ptulsconv/docparser/adr_entity.py
+++ ptulsconv/docparser/adr_entity.py
@@ -1,6 +1,7 @@
 from .doc_entity import SessionDescriptor
 from .tagged_string_parser_visitor import parse_tags
 
+from fractions import Fraction
 # field_map maps tags in the text export to fields in FMPXMLRESULT
 #  - tuple field 0 is a list of tags, the first tag with contents will be used as source
 #  - tuple field 1 is the field in FMPXMLRESULT
@@ -40,6 +41,33 @@ adr_field_map = ((['Title', 'PT.Session.Name'], 'Title', str),
                  (['Movie.Filename'], 'Movie', str),
                  (['Movie.Start_Offset_Seconds'], 'Movie Seconds', float),
                  )
+
+
+class ADRLine:
+    title: str
+    supervisor: str
+    client: str
+    scene: str
+    version: str
+    reel: str
+    start: str
+    finish: str
+    priority: int
+    cue_number: str
+    character_id: str
+    prompt: str
+    reason: str
+    requested_by: str
+    note: str
+    spot: str
+    shot: str
+    effort: bool
+    tv: bool
+    tbw: bool
+    omitted: bool
+    adlib: bool
+    optional: bool
+    done: bool
 
 
 class ADRDocument:

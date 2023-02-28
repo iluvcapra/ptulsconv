ddf1948f3cb75742158265df63519a47fdd1be9d
Jamie Hardt <jamiehardt@me.com>
2023-02-28 10:40:57 -0800

    Upgraded to pyproject/flit build style

--- ptulsconv/__init__.py
+++ ptulsconv/__init__.py
@@ -1,4 +1,4 @@
 __version__ = '1.0.5'
-__author__  = 'Jamie Hardt'
-__license__ = 'MIT'
-__copyright__ = "%s %s (c) 2022 %s. All rights reserved." % (__name__, __version__, __author__)
+# __author__  = 'Jamie Hardt'
+# __license__ = 'MIT'
+# __copyright__ = "%s %s (c) 2022 %s. All rights reserved." % (__name__, __version__, __author__)

5427b4cfb16911745a23e9230ea1faaa8f30ef78
Jamie Hardt <jamiehardt@me.com>
2023-06-02 19:25:07 -0700

    BUmped version number and copyright

--- ptulsconv/__init__.py
+++ ptulsconv/__init__.py
@@ -2,7 +2,7 @@
 Parse and convert Pro Tools text exports
 """
 
-__version__ = '1.0.6'
+__version__ = '1.0.7'
 __author__  = 'Jamie Hardt'
 __license__ = 'MIT'
-__copyright__ = "%s %s (c) 2022 %s. All rights reserved." % (__name__, __version__, __author__)
+__copyright__ = "%s %s (c) 2023 %s. All rights reserved." % (__name__, __version__, __author__)

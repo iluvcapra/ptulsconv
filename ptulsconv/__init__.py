17b87b6e696fac442cb70e788fb2e16e8f5b240f
Jamie Hardt <jamiehardt@me.com>
2023-07-27 23:23:39 -0700

    Update __init__.py
    Nudged version

--- ptulsconv/__init__.py
+++ ptulsconv/__init__.py
@@ -2,7 +2,7 @@
 Parse and convert Pro Tools text exports
 """
 
-__version__ = '2.0.1'
+__version__ = '2.1.0'
 __author__ = 'Jamie Hardt'
 __license__ = 'MIT'
 __copyright__ = "%s %s (c) 2023 %s. All rights reserved." \

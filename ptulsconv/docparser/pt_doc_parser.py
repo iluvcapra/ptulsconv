a602b09551e0818e584776b8721cb78d60514d77
Jamie Hardt <jamiehardt@me.com>
2025-05-17 10:47:21 -0700

    flake8

--- ptulsconv/docparser/pt_doc_parser.py
+++ ptulsconv/docparser/pt_doc_parser.py
@@ -86,7 +86,7 @@ protools_text_export_grammar = Grammar(
                              "COMMENTS" rs
 
     marker_record = integer_value isp fs string_value fs integer_value isp fs
-                    string_value fs string_value fs (string_value fs 
+                    string_value fs string_value fs (string_value fs
                     string_value fs)? string_value rs
 
     fs = "\t"

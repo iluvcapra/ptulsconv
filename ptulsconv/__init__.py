f847b88aa32553ca9bb90cd4de3849a513a82965
Jamie Hardt <jamiehardt@me.com>
2025-05-17 12:06:56 -0700

    Nudged version and copyright date

--- ptulsconv/__init__.py
+++ ptulsconv/__init__.py
@@ -2,8 +2,8 @@
 Parse and convert Pro Tools text exports
 """
 
-__version__ = '2.1.1'
+__version__ = '2.2.0'
 __author__ = 'Jamie Hardt'
 __license__ = 'MIT'
-__copyright__ = "%s %s (c) 2023 %s. All rights reserved." \
+__copyright__ = "%s %s (c) 2025 %s. All rights reserved." \
         % (__name__, __version__, __author__)

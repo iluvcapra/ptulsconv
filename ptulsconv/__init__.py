630e7960dc0ca0bf99a6254a0e73e716e1a94c37
Jamie Hardt <jamiehardt@me.com>
2025-05-24 22:20:15 -0700

    Making changes for peotry

--- ptulsconv/__init__.py
+++ ptulsconv/__init__.py
@@ -2,8 +2,3 @@
 Parse and convert Pro Tools text exports
 """
 
-__version__ = '2.2.1'
-__author__ = 'Jamie Hardt'
-__license__ = 'MIT'
-__copyright__ = "%s %s (c) 2025 %s. All rights reserved." \
-        % (__name__, __version__, __author__)
